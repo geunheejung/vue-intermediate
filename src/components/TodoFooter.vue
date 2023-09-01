8ae8dc53c72aa19b2d22ab0d1119b704fff4cdc9
geunheejung <geunhee0212@gmail.com>
2023-09-01 19:44:34 +0900

    TodoFooter 구현.

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>Footer</div>
+  <div class="clear-all-container" @click="clearTodo">
+    <span class="clear-all-btn">Clear All</span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    clearTodo: function () {
+      localStorage.clear();
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+.clear-all-container {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background: #fff;
+  border-radius: 5px;
+  margin: 1rem auto 0;
+}
+.clear-all-btn {
+  color: #e20303;
+  display: block;
+}
+</style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -42,7 +42,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .input-box input:focus {
   outline: none;
 }
